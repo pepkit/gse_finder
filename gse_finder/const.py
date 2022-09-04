fa61f14818a7cbf047ab6fe9368207db0c979455
Khoroshevskyi <sasha99250@gmail.com>
2022-09-04 14:23:18 -0400

    First working version

---
+++ gse_finder/const.py
@@ -0,0 +1,14 @@
+RETMAX = 10000000 # once it should be increased
+
+# gds = geo DataSets
+ETOOLS_GEO_BASE = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds'
+ETOOLS_GEO_GSE_BASE = f'{ETOOLS_GEO_BASE}&term=GSE[ETYP]'
+
+ETOOLS_ENDING = '&retmax={retmax}&usehistory=y'
+
+ETOOLS_GEO_GSE_GET_ALL = ETOOLS_GEO_GSE_BASE
+
+TODAY_DATE = '3000'
+
+DATE_FILTER = '+AND+("{start_date}"[Publication%20Date]%20:%20"{end_date}"[Publication%20Date])'
+THREE_MONTH_FILTER = '+AND+"published+last+3+months"[Filter]'
